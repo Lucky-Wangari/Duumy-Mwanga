77de847383e136f0881a0d63f1cc99f3eaf5b628
Lucky-Wangari <wangarimarylucky@gmail.com>
2023-09-05 14:27:02 +0300

    added hover on a single card

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
 import "./style.css";
-import register from "../../assets/images/register.png" 
 
 const Hoover = () => {
-  const [onHover, setOnHover] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
-  const handleHover = () => {
-    setOnHover(true);
+  const handleHover = (index) => {
+    setHoveredIndex(index);
   };
 
   const handleNotHover = () => {
-    setOnHover(false);
+    setHoveredIndex(null);
   };
 
   return (
-    <div
-      className={`details-container ${onHover ? "hovered" : ""}`}
-      onMouseOver={handleHover}
-      onMouseOut={handleNotHover}
-    >
+    <div className="details-container">
       {detail.map((item, index) => (
-        <div className="detail-box" key={index}>
-          <h3>{item.title}</h3>
-          {onHover && <p>{item.description}</p>}
+        <div
+          className={`detail-box ${hoveredIndex === index ? "hovered" : ""}`}
+          key={index}
+          onMouseOver={() => handleHover(index)}
+          onMouseOut={handleNotHover}
+        >
+          <div className="icon">{item.icon}</div>
+          <div className="title">{item.title}</div>
+          {hoveredIndex === index && <p>{item.description}</p>}
         </div>
       ))}
     </div>
@@ -34,21 +35,25 @@ export const detail = [
     title: "Registration",
     description:
       "Children identified to suffer from delayed milestones are registered and referred to the hospital for treatment.",
+    icon: <i className="fas fa-user" />,
   },
   {
     title: "Tracking",
     description:
       "After treatment for two weeks, the children's progress to recovery is tracked to know if they are improving.",
+    icon: <i className="fas fa-chart-line" />,
   },
   {
     title: "Data",
     description:
       "Raw data is collected, analyzed, and displayed on a dashboard to give insights on the same, thus enabling NGOs to effectively allocate resources.",
+    icon: <i className="fas fa-database" />,
   },
   {
     title: "Geolocation",
     description:
       "Geographically tagging the affected areas and areas that are improving from receiving aid from the NGO.",
+    icon: <i className="fas fa-map-marker-alt" />,
   },
 ];
 
